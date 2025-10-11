8caaeeba2eb71589330465db0d5adb6d0058724a
margaretpalma <mpalma1496@gmail.com>
2025-10-11 12:48:58 -0400

    payment menu works

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import javax.xml.transform.Source;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -19,20 +20,35 @@ public class Main {
 
             switch (choice) {
                 case "D":
-                    System.out.println("Deposit");
+                    System.out.println("-- Add Deposit--");
                     LocalDate date = ConsoleHelper.promptForDate("Enter Date |yyyy-mm-dd|");
                     LocalTime time = ConsoleHelper.promptForTime("Enter Time |hh:mm:ss");
                     String description = ConsoleHelper.promptForString("Enter Description");
                     String vendor = ConsoleHelper.promptForVendor("Enter Vendor");
                     Double amount = ConsoleHelper.promptForAmount("Enter Amount");
                     transactions.add(new Transactions(date, time, description,vendor, amount));
-                    System.out.println(date + "|" + time + "|" + description + "|" + vendor + "|" + "$" + amount);
+                    System.out.println(date + "|" + time + "|" + description +"|"+ vendor +"|$"+ amount);
                     System.out.println("--Deposit Added--");
+
                     break;
                 case "P":
-                    System.out.println("Payment");
+                    System.out.println("-- Make A Payment--");
+                    LocalDate paymentDate = ConsoleHelper.promptForDate("Enter Date |yyyy-mm-dd|");
+                    LocalTime paymentTime = ConsoleHelper.promptForTime("Enter Time |hh:mm:ss");
+                    String paymentDescription = ConsoleHelper.promptForString("Enter Description");
+                    String paymentVendor = ConsoleHelper.promptForVendor("Enter Vendor");
+                    Double paymentAmount = ConsoleHelper.promptForAmount("Enter Amount");
+
+                    //payment = negative
+                    paymentAmount = -Math.abs(paymentAmount);
+                    transactions.add(new Transactions(paymentDate,paymentTime,paymentDescription, paymentVendor,paymentAmount));
+                    System.out.println(paymentDate + "|" + paymentTime + "|" + paymentDescription + "|" + paymentVendor + "|$" + paymentAmount );
+                    System.out.println("--Payment Complete--");
+
+
                     break;
                 case "L":
+
                     System.out.println("Ledger");
                     break;
                 case "X":
@@ -54,14 +70,9 @@ public class Main {
 
 
 //D) Add Deposit P)Make Payment(Debit) L)Ledger X)exit
-
-
-
 //todo: call console ledger
 //todo: bufferedReader, fileReader
-
 //todo: switch statement
 //todo: exception catching
 //todo: return transactions
-
 //todo: display of menus, menu within menu
